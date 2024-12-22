2b5692dd144f7035e8a814038f982f5e71ca1c14
Liu Yue <euyuil@gmail.com>
2024-12-22 12:03:33 +0800

    Update routes for security indices

--- app/main/routes.py
+++ app/main/routes.py
@@ -21,6 +21,101 @@ def products():
     """可投产品首页（列出四大类链接）"""
     return render_template("products/index.html")
 
+@main_bp.route("/products/funds")
+def products_funds():
+    """公募基金页面"""
+    return render_template("products/funds/index.html")
+
+@main_bp.route("/products/funds/fof")
+def products_funds_fof():
+    """养老FOF页面"""
+    funds = TargetDateFund.query.all()
+    return render_template("products/funds/fof.html", funds=funds)
+
+@main_bp.route("/products/funds/indexfunds")
+def products_funds_indexfunds():
+    """指数基金页面"""
+    funds = IndexFund.query.all()
+    return render_template("products/funds/indexfunds.html", funds=funds)
+
+@main_bp.route("/products/funds/enhanced")
+def products_funds_enhanced():
+    """增强指数基金页面"""
+    funds = EnhancedIndexFund.query.all()
+    return render_template("products/funds/enhanced.html", funds=funds)
+
+@main_bp.route("/products/funds/<sub_category>/fund/<fund_code>")
+def single_fund(sub_category, fund_code):
+    """单只基金页面"""
+    if sub_category not in ["fof", "indexfunds", "enhanced"]:
+        abort(404)
+    fund = Fund.query.filter(Fund.code == fund_code).first()
+    if not fund:
+        abort(404)
+    return render_template("products/funds/single_fund.html", sub_category=sub_category, fund=fund)
+
+@main_bp.route("/products/funds/research/indices")
+def products_funds_research_indices():
+    """基准指数页面"""
+    indices = Security.query.filter(Security.type.like("TI%")).all()
+    return render_template("products/funds/research/indices.html", indices=indices)
+
+@main_bp.route("/products/funds/research/indices/<index_code>")
+def products_funds_research_index_detail(index_code):
+    """基准指数详细页面"""
+    security = Security.query.filter(Security.code == index_code).first()
+    if security is None:
+        return "No such index found", 404
+    if security.type[:2] != "TI":
+        return "This is not an index", 400
+    code = security.code
+
+    index_data = data_manager.get_data(
+        start="2024-01-01",
+        end="2024-12-31",
+        frequency="1d",
+        securities=[code],
+        fields=["Close"]
+    )
+
+    dates = [date.strftime("%Y-%m-%d") for date in pd.to_datetime(index_data.datetime)]
+    closing_prices = [price.item() for price in index_data["Close"].values]
+
+    return render_template(
+        "products/funds/research/index_detail.html",
+        security=security,
+        dates=dates,
+        closing_prices=closing_prices
+    )
+
+@main_bp.route("/products/funds/research/managers")
+def products_funds_research_managers():
+    """基金经理介绍页面"""
+    managers = list(get_all_research('managers'))
+    return render_template("products/funds/research/managers.html", managers=managers)
+
+@main_bp.route("/products/funds/research/managers/<manager_id>")
+def products_funds_research_manager_detail(manager_id):
+    """基金经理详细页面"""
+    manager = get_research_data('managers', manager_id)
+    if not manager:
+        abort(404)
+    return render_template("products/funds/research/manager_detail.html", manager=manager)
+
+@main_bp.route("/products/funds/research/management")
+def products_funds_research_management():
+    """管理公司介绍页面"""
+    management_companies = list(get_all_research('management'))
+    return render_template("products/funds/research/management.html", management_companies=management_companies)
+
+@main_bp.route("/products/funds/research/management/<management_id>")
+def products_funds_research_management_detail(management_id):
+    """管理公司详细页面"""
+    management = get_research_data('management', management_id)
+    if not management:
+        abort(404)
+    return render_template("products/funds/research/management_detail.html", management=management)
+
 @main_bp.route("/products/bank")
 def products_bank():
     """展示银行理财相关内容"""
@@ -81,70 +176,7 @@ def products_insurance_terms():
     """商业养老保险的条款解读"""
     return render_template("products/insurance/terms.html")
 
-@main_bp.route("/products/funds")
-def products_funds():
-    """公募基金页面"""
-    return render_template("products/funds/index.html")
-
-@main_bp.route("/products/funds/fof")
-def products_funds_fof():
-    """养老FOF页面"""
-    funds = TargetDateFund.query.all()
-    return render_template("products/funds/fof.html", funds=funds)
-
-@main_bp.route("/products/funds/indexfunds")
-def products_funds_indexfunds():
-    """指数基金页面"""
-    funds = IndexFund.query.all()
-    return render_template("products/funds/indexfunds.html", funds=funds)
-
-@main_bp.route("/products/funds/enhanced")
-def products_funds_enhanced():
-    """增强指数基金页面"""
-    funds = EnhancedIndexFund.query.all()
-    return render_template("products/funds/enhanced.html", funds=funds)
-
-@main_bp.route("/products/funds/<sub_category>/fund/<fund_code>")
-def single_fund(sub_category, fund_code):
-    """单只基金页面"""
-    fund = Fund.query.filter(Fund.code == fund_code).first()
-    if not fund:
-        abort(404)
-    return render_template("products/funds/single_fund.html", sub_category=sub_category, fund=fund)
-
 @main_bp.route("/about")
 def about():
     """关于我们页面"""
     return render_template("about.html")
-
-@main_bp.route("/Indices")
-def indices():
-    indices = Security.query.filter(Security.type.like("TI%")).all()
-    return render_template("indices.html", indices=indices)
-
-@main_bp.route("/Indices/<code>")
-def sec_index(code):
-    security = Security.query.filter(Security.code == code).first()
-    if security is None:
-        return "No such index found", 404
-    if security.type[:2] != "TI":
-        return "This is not an index", 400
-    code = security.code
-
-    index_data = data_manager.get_data(
-        start="2024-01-01",
-        end="2024-12-31",
-        frequency="1d",
-        securities=[code],
-        fields=["Close"]
-    )
-
-    dates = [date.strftime("%Y-%m-%d") for date in pd.to_datetime(index_data.datetime)]
-    closing_prices = [price.item() for price in index_data["Close"].values]
-
-    return render_template(
-        "sec_index.html",
-        security=security,
-        dates=dates,
-        closing_prices=closing_prices
-    )
